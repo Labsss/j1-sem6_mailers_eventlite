e1d1b6795f0b7b21eef4e4259d4874412edae0a3
Labsss <alexandre.labonne@gmail.com>
2019-05-06 18:16:40 +0200

    good ?

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
     has_many :attendances
 
     validates :start_date, presence: true  #(:start_date_cannot_be_in_the_past)
-    validates :duration, presence: true, numericality: true, greater_than: { 0 }
+    validates :duration, presence: true, numericality: true, greater_than: 0 
     validate :multiple_de_5
     validates :title, presence: true, length: { in: 5..140 }
     validates :description, presence: true, length: { in: 20..1000 }
